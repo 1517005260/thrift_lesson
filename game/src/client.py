df2598be52ba9b1ab5e0b8cc2eeafffb80092477
glk <1517005260@qq.com>
2023-12-14 15:07:30 +0800

    Finished match client

--- game/src/client.py
+++ game/src/client.py
@@ -6,9 +6,9 @@ from thrift import Thrift
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
+from sys import stdin
 
-
-def main():
+def operate(op,user_id,username,score):
     # Make socket
     transport = TSocket.TSocket('localhost', 9090)
 
@@ -26,12 +26,21 @@ def main():
 
     ##接下来写你的代码：
 
-    user = User(1,'glk',1500)
-    client.add_user(user,"")
+    user = User(user_id,username,score)
+
+    if op == "add" :
+        client.add_user(user,"")
+    elif op == "remove" :
+        client.remove_user(user,"")
 
     # Close!
     transport.close()
 
+
+def main():
+    for line in stdin :
+        op,user_id,username,score = line.split(' ')  ##读入4个参数，以空格隔开
+        operate(op,int(user_id),username,int(score))
 
 
 if __name__ == "__main__" :
